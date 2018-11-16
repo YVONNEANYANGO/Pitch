60137c4df7a64a59bbf8897118bafd418a1a375e
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-16 16:39:10 +0300

    add comments file that will host the creation of class comments

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import render_template
 from app import app
 
-
 def index():
 
     '''
@@ -15,11 +14,14 @@ def index():
 @app.route('/')
 def index():
 
+    
+
     '''
     View root page function that returns the index page and its data
     '''
 
     message = 'Welcome To OneMinutePitch!!!'
+    print(message)
     return render_template('index.html',message = message)
 
 @app.route('/pitch/<int:pitch_id>')
