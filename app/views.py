8d856599f5ba680afd127939bb03a1b255bb0fb2
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-16 12:36:35 +0300

    add flask_bootstrap to the application

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from flask import render_template
 from app import app
 
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to One-Minute-Pitch Website Online'
+    return render_template('index.html', title = title)
+
 # Views
 @app.route('/')
 def index():
