d23f2d1d44d6fe177e5229e8e2825776f13ddfd8
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-19 16:44:36 +0300

    Installed flask-login to help in managing the user authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,13 @@ from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from .config import config_options
+from flask_login import LoginManager
 # from app import views
 # from app import error
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 # from app import views
 # return app
@@ -21,9 +26,13 @@ def create_app(config_name):
     bootstrap.init_app(app)
     # db = SQLAlchemy()
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
