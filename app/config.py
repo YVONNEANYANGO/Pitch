5b724984518d2bb636340a3cfa6bf5111d51619e
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-22 17:54:45 +0300

    config file

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:valqueen@localhost/pitch'
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:valqueen@localhost/pitch'
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
