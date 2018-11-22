3c2c91998829bc78b719442b447c0e70766f9d9c
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-22 19:23:17 +0300

    resolving log tails errors

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 
 export MAIL_USERNAME="ivonnahbonswuh@gmail.com"
 export MAIL_PASSWORD="valqueen"
-export SECRET_KEY=ivyvalencialrt
+
+export SECRET_KEY="ivyvalencialrt"
 
 
 python3.6 manage.py server
